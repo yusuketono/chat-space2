d4c5d9201a78b37c42172912052d2139f9c2b1be
yusuketono <yusuke.tono@di-v.co.jp>
2019-10-12 16:19:43 +0900

    7. 作成したグループをサイドバーに表示する

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -15,6 +18,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
+    params.require(:group).permit(:name, user_ids: [] )
   end
 end
