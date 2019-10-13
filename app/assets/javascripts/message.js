f0c907ab1c052d1aac5f65e1659bc74bf98bc15a
yusuketono <yusuke.tono@di-v.co.jp>
2019-10-13 23:52:37 +0900

    6.jbuilderを使用して、作成したメッセージをJSON形式で返す

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,14 @@
 $(document).on('turbolinks:load', function(){
-  $('#new_message').on('submit', function(){
-    console.log('これはコンソールログです');
+  $('#new_message').on('submit', function(e){
+    let message = new FormData(this);
+    let url = $(this).attr('action')
+    // console.log(url);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: message,
+      processData: false,
+      contentType: false
+    })
   })
 });
