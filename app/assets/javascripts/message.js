ba9c5e0bbd68b469419dc3c014b37e94077f6d8c
yusuketono <yusuke.tono@di-v.co.jp>
2019-10-14 02:34:22 +0900

    とりあえず

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,30 @@
 $(document).on('turbolinks:load', function(){
+  function buildHTML(message){
+    let content = message.content ? '${ message.content }': "";
+    let img = message.image ? '<img src = ${message.image}>' :"";
+    let html = `<div class="message" data-id="${message.id}">
+                  <div class="message__info">
+                    <div class="message__info__member">
+                      ${message.user_name}
+                    </div>
+                    <p class="message__info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
+    e.preventDefault();
     let message = new FormData(this);
     let url = $(this).attr('action')
-    // console.log(url);
     $.ajax({
       url: url,
       type: "POST",
@@ -10,5 +32,14 @@ $(document).on('turbolinks:load', function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    });
+    return false;
   })
 });
