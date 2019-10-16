4b9ddb20c1fdfc769f7586fe416103b3668e253d
yusuketono <yusuke.tono@di-v.co.jp>
2019-10-16 11:13:24 +0900

    2. ルーティングなどAPI側の準備をする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+def index
+  @users = User.search_for_group(params)
+  respond_to do |format|
+    format.html
+    format.json
+  end
+end
+
   def edit
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
+
+  def self.search_for_group(params)
+    where("name LIKE(?)", "%#{params[:keuword]}%").where.not(id: params[:user_ids])
+  end
 end
