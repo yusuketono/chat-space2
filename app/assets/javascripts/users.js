837207642a1fbe252c1e58f03a679b56bfb4e5e4
yusuketono <yusuke.tono@di-v.co.jp>
2019-10-17 20:47:16 +0900

    a

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,17 @@
 $(document).on('turbolinks:load', function(){
+
   $('#user-search-field').on('input', function(e){
-    console.log('発火しているか確認する')
+    e.preventDefault();
+    let input = $('#user-search-field').val();
+    if (input.length == 0) {
+      user_list.empty();
+      return
+    };
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      dataType: 'json',
+      data: {keyword:input},
+    })
   });
 });
