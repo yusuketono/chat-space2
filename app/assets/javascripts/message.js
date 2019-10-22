9890b1ccb0e11b410acd3423f033ee8e801962f0
yusuketono <yusuke.tono@di-v.co.jp>
2019-10-22 11:17:13 +0900

    4.追加したアクションをリクエストするよう実装しよう

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,7 +20,7 @@ $(document).on('turbolinks:load', function(){
                 </div>`
     return html;
   }
-  
+  // メッセージ送信
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     let message = new FormData(this);
@@ -43,4 +43,29 @@ $(document).on('turbolinks:load', function(){
     });
     return false;
   })
+
+  // 自動更新
+  $(function() {
+    //省略
+    const reloadMessages = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.message:last').data('id');
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: "api/messages",
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        console.log('success');
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+  });
+
 });
