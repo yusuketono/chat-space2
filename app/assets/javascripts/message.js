11b7bc7684fe2743b8955fe9ba8bc7be4d954032
yusuketono <yusuke.tono@di-v.co.jp>
2019-10-25 11:03:24 +0900

    aaaa

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,6 +20,15 @@ $(document).on('turbolinks:load', function(){
                 </div>`
     return html;
   }
+
+  // function scrollBottom(){
+  //   var target = $('.message').last();
+  //   var position = target.offset().top + $('.messages').scrollTop();
+  //   $('.messages').animate({
+  //     scrollTop: position
+  //   }, 300, 'swing');
+  // }
+
   // メッセージ送信
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -36,7 +45,9 @@ $(document).on('turbolinks:load', function(){
     .done(function(data){
       let html = buildHTML(data);
       $('.messeges').append(html);
-      $('form')[0].reset();  
+      $('form')[0].reset(); 
+      // scrollBottom();
+      // console.log(html);
     })
     .fail(function(){
       alert('error');
@@ -45,35 +56,44 @@ $(document).on('turbolinks:load', function(){
   })
 
   // 自動更新
-  $(function() {
+
     //省略
-    const reloadMessages = function() {
-      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-      last_message_id = $('.message:last').data('id');
-      $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-        url: "api/messages",
-        //ルーティングで設定した通りhttpメソッドをgetに指定
-        type: 'get',
-        dataType: 'json',
-        //dataオプションでリクエストに値を含める
-        data: {id: last_message_id}
+  const reloadMessages = function() {
+    if($('.messages')[0]){
+      var last_message_id = $('.message:last').data('id');
+    } 
+    else {
+      var last_message_id = 0
+    }
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      // console.log(messages);
+      //追加するHTMLの入れ物を作る
+      let insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      messages.forEach(function(message){
+      //メッセージが入ったHTMLを取得
+      insertHTML = buildHTML(message);
+      // console.log(insertHTML);
+      //メッセージを追加
+      $('.messages').append(insertHTML);
+      // scrollBottom();
       })
-      .done(function(messages) {
-        //追加するHTMLの入れ物を作る
-        let insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        messages.forEach(function(message){
-        //メッセージが入ったHTMLを取得
-        insertHTML = buildHTML(message);
-        //メッセージを追加
-        $('.messages').append(insertHTML);
-        })
-      })
-      .fail(function() {
-        console.log('error');
-      });
-    };
-    setInterval(reloadMessages, 5000);
-  });
+    })
+    .fail(function() {
+      alert('自動更新に失敗しました');
+    })
+    }
+  }
+  setInterval(reloadMessages, 5000);
 });
